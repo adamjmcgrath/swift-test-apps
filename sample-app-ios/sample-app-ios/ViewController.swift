5398608c6a8281a56dc0138ed73065f8cd93f107
adamjmcgrath <adam.mcgrath@auth0.com>
2022-01-14 13:17:49 -0400

    update

--- sample-app-ios/sample-app-ios/ViewController.swift
+++ sample-app-ios/sample-app-ios/ViewController.swift
@@ -11,18 +11,21 @@ import Auth0
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+    }
+
+    @IBAction func onClick(_ sender: UIButton) {
         Auth0
             .webAuth()
             .logging(enabled: true)
             .start { result in
                 switch result {
                 case .success(let credentials):
-                    print(credentials)
+                    let alert = UIAlertController(title: "Alert", message: credentials.idToken, preferredStyle: .alert)
+                    self.present(alert, animated: true, completion: nil)
                 case .failure(let error):
                     print("Failed with: \(error)")
                 }
             }
     }
-
-
+    
 }
